a981bd5cc1489cb4b75ae7ce5173c5068c309a1c
maxiaotong <2607173480@qq.com>
2023-03-15 21:55:52 +0800

    优化

--- src/api/pro.js
+++ src/api/pro.js
@@ -1,6 +1,6 @@
 import ajax from '../utils/request';
 // 获取商品接口
-export function proList (params) {
+export function proList () {
     return ajax({
         url: 'pro/list'
     })
--- src/views/pro/ProListView.vue
+++ src/views/pro/ProListView.vue
@@ -1,5 +1,5 @@
+
+
 <template>
-    <div>
-        商品列表
-    </div>
+     <div>chanping1</div>
 </template>
